e3982464bf0ac5c0d5d0c0c9fcb477a911774320
Thomas Stenner <thomasstenner@thomass-mbp.attlocal.net>
2021-06-19 10:23:07 -0500

    new

--- static/js/logic.js
+++ static/js/logic.js
@@ -18,3 +18,30 @@ function createFeatures(earthquakeData) {
       layer.bindPopup("<h3>" + feature.properties.place +
         "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
     }
+
+// Define function to create the circle radius based on the magnitude
+function radiusSize(magnitude) {
+    return magnitude * 20000;
+  }
+
+  // Define function to set the circle color based on the magnitude
+  function circleColor(magnitude) {
+    if (magnitude < 1) {
+      return "#ccff33"
+    }
+    else if (magnitude < 2) {
+      return "#ffff33"
+    }
+    else if (magnitude < 3) {
+      return "#ffcc33"
+    }
+    else if (magnitude < 4) {
+      return "#ff9933"
+    }
+    else if (magnitude < 5) {
+      return "#ff6633"
+    }
+    else {
+      return "#ff3333"
+    }
+  }
